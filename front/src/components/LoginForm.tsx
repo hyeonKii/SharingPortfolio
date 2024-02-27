75d453c3d0b748441f51d62fae255e832e0d0053
hyeonKii <khk5676@gmail.com>
2024-02-27 23:36:53 +0900

    feat: loginform css 작업 중(80%)

--- front/src/components/LoginForm.tsx
+++ front/src/components/LoginForm.tsx
@@ -69,16 +69,17 @@ export function LoginForm() {
             id="login__wrap"
             className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
         >
+            <h1 id="main__title" className="text-3xl text-white mb-5">sharing portfolio</h1>
             <div
                 id="login__block"
-                className="w-full h-2/3 bg-transparent backdrop-blur-2xl rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0"
+                className="w-full h-3/5 bg-transparent backdrop-blur-3xl rounded-xl shadow-2xl md:mt-0 sm:max-w-md xl:p-0"
             >
-                <h1
+                <h2
                     id="form__title"
-                    className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl"
+                    className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 my-10 md:text-2xl"
                 >
                     로그인
-                </h1>
+                </h2>
                 <form
                     className="space-y-4 md:space-y-6"
                     onSubmit={handleSubmit}
@@ -86,12 +87,12 @@ export function LoginForm() {
                     <div>
                         <label
                             htmlFor="email"
-                            className="block mb-2 text-sm font-medium text-gray-900"
+                            className="block mb-2 text-sm font-medium text-gray-900 ml-12"
                         >
                             이메일
                         </label>
                         <input
-                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-4/5 p-2.5 mx-auto"
                             type="email"
                             name="email"
                             id="email"
@@ -103,12 +104,12 @@ export function LoginForm() {
                     <div>
                         <label
                             htmlFor="password"
-                            className="block mb-2 text-sm font-medium text-gray-900"
+                            className="block mb-2 text-sm font-medium text-gray-900 ml-12"
                         >
                             비밀번호
                         </label>
                         <input
-                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-4/5 p-2.5 mx-auto"
                             type="password"
                             name="password"
                             id="password"
@@ -117,7 +118,7 @@ export function LoginForm() {
                             required
                         />
                     </div>
-                    <div className="flex items-center justify-between">
+                    <div className="flex items-center justify-around">
                         <div className="flex items-start">
                             계정이 없으신가요?
                         </div>
@@ -138,7 +139,7 @@ export function LoginForm() {
                         <button
                             type="submit"
                             id="form__btn__submit"
-                            className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                            className="w-4/5 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-12 mt-1"
                         >
                             로그인
                         </button>
