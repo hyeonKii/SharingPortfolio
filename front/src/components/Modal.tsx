e61f39ecfec2093f86e1c88ead46fba347995706
hyeonKii <khk5676@gmail.com>
2024-03-15 23:47:09 +0900

    feat: 모달 설계 시나리오 수정

--- front/src/components/Modal.tsx
+++ front/src/components/Modal.tsx
@@ -16,4 +16,5 @@ export function Modal({children}: IModal) {
 //portfolio 컴포넌트에서 모달 오픈 및 비오픈 상태를 정의하고
 // 해당 상태에 따라 모달 백그라운드를 여는 것을 만들 필요가 있음
 // 그런데 문제는 isAdd setIsAdd는 자식 컴포넌트에 선언된 상태다
-// add라는 상태를 전역 상태로 수정하는 것이 필요
+// add라는 상태를 portfolio에 정의해서 setIsAdd를 각 컴포넌트에 props로 전달
+// 전달받은 setIsAdd는 + 버튼을 누르면 호출하도록 설정
